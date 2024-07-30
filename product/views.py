fd4cbbc071e9067b072b39bc65065fadbd7d504c
ikinoti <gitongakinoti1@gmail.com>
2024-07-31 02:48:02 +0300

    viewset to list all the brands

--- product/views.py
+++ product/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
 
+@extend_schema(responses=CategorySerializer)
 class CategoryViewSet(viewsets.ViewSet):
     '''
-    A simpple Viewset for viewing all categories
+    A simple Viewset for viewing all categories
     '''
-
 
     queryset = Category.objects.all()
 
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+@extend_schema(responses=BrandSerializer)
+class BrandViewSet(viewsets.ViewSet):
+    '''
+    A simple Viewset for viewing all brand
+    '''
+
+    queryset = Brand.objects.all()
+
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
