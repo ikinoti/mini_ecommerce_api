01867060e6b68dd23d01d38145ea885dd26b23f3
ikinoti <gitongakinoti1@gmail.com>
2024-08-11 15:53:17 +0300

    endpoint

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from . import views
 
 router = DefaultRouter()
 router.register(r"category", views.CategoryViewSet)
+router.register(r"brand", views.BrandViewSet)
+router.register(r"product", views.ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
