1cbdeb031a7e3204f45a23bfb5ac030892a1ea5b
ikinoti <gitongakinoti1@gmail.com>
2024-08-11 15:51:22 +0300

    modified

--- ecommerce_API/settings.py
+++ ecommerce_API/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
+
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +130,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'E-Commerce API',
+}
